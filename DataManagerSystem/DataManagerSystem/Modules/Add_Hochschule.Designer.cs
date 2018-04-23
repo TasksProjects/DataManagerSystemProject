25d92b312796ac9da5ae1e85baae1b3b40aec9b9
francpouhela <francpouhela@yahoo.fr>
2018-04-23 09:03:18 +0200

    some small changes

--- DataManagerSystem/DataManagerSystem/Modules/Add_Hochschule.Designer.cs
+++ DataManagerSystem/DataManagerSystem/Modules/Add_Hochschule.Designer.cs
@@ -28,17 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Hochschule));
             this.NameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SaveBtn = new System.Windows.Forms.Button();
             this.Cancelbtn = new System.Windows.Forms.Button();
             this.LandComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bunifuSeparator2 = new Bunifu.Framework.UI.BunifuSeparator();
             this.SuspendLayout();
             // 
             // NameTextBox
             // 
-            this.NameTextBox.Location = new System.Drawing.Point(158, 42);
+            this.NameTextBox.Location = new System.Drawing.Point(158, 73);
             this.NameTextBox.Name = "NameTextBox";
             this.NameTextBox.Size = new System.Drawing.Size(225, 20);
             this.NameTextBox.TabIndex = 0;
@@ -47,7 +50,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Location = new System.Drawing.Point(12, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(131, 16);
             this.label1.TabIndex = 1;
@@ -57,7 +60,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Location = new System.Drawing.Point(12, 124);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(123, 16);
             this.label2.TabIndex = 2;
@@ -69,9 +72,9 @@
             this.SaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SaveBtn.ForeColor = System.Drawing.Color.White;
-            this.SaveBtn.Location = new System.Drawing.Point(15, 162);
+            this.SaveBtn.Location = new System.Drawing.Point(158, 173);
             this.SaveBtn.Name = "SaveBtn";
-            this.SaveBtn.Size = new System.Drawing.Size(136, 32);
+            this.SaveBtn.Size = new System.Drawing.Size(112, 32);
             this.SaveBtn.TabIndex = 3;
             this.SaveBtn.Text = "Save";
             this.SaveBtn.UseVisualStyleBackColor = false;
@@ -79,13 +82,13 @@
             // 
             // Cancelbtn
             // 
-            this.Cancelbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.Cancelbtn.BackColor = System.Drawing.Color.White;
             this.Cancelbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Cancelbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cancelbtn.ForeColor = System.Drawing.Color.White;
-            this.Cancelbtn.Location = new System.Drawing.Point(247, 162);
+            this.Cancelbtn.ForeColor = System.Drawing.Color.Black;
+            this.Cancelbtn.Location = new System.Drawing.Point(271, 173);
             this.Cancelbtn.Name = "Cancelbtn";
-            this.Cancelbtn.Size = new System.Drawing.Size(136, 32);
+            this.Cancelbtn.Size = new System.Drawing.Size(112, 32);
             this.Cancelbtn.TabIndex = 4;
             this.Cancelbtn.Text = "Cancel";
             this.Cancelbtn.UseVisualStyleBackColor = false;
@@ -94,26 +97,54 @@
             // LandComboBox
             // 
             this.LandComboBox.FormattingEnabled = true;
-            this.LandComboBox.Location = new System.Drawing.Point(158, 92);
+            this.LandComboBox.Location = new System.Drawing.Point(158, 123);
             this.LandComboBox.Name = "LandComboBox";
             this.LandComboBox.Size = new System.Drawing.Size(225, 21);
             this.LandComboBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 24);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Add Hochschule";
+            // 
+            // bunifuSeparator2
+            // 
+            this.bunifuSeparator2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.bunifuSeparator2.LineThickness = 2;
+            this.bunifuSeparator2.Location = new System.Drawing.Point(12, 32);
+            this.bunifuSeparator2.Name = "bunifuSeparator2";
+            this.bunifuSeparator2.Size = new System.Drawing.Size(413, 10);
+            this.bunifuSeparator2.TabIndex = 21;
+            this.bunifuSeparator2.Transparency = 255;
+            this.bunifuSeparator2.Vertical = false;
             // 
             // Add_Hochschule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(405, 216);
+            this.ClientSize = new System.Drawing.Size(437, 253);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bunifuSeparator2);
             this.Controls.Add(this.LandComboBox);
             this.Controls.Add(this.Cancelbtn);
             this.Controls.Add(this.SaveBtn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.NameTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Add_Hochschule";
-            this.Text = "Add_Hochschule";
+            this.Text = "Add New Hochschule";
             this.Load += new System.EventHandler(this.Add_Hochschule_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -128,5 +159,7 @@
         private System.Windows.Forms.Button SaveBtn;
         private System.Windows.Forms.Button Cancelbtn;
         private System.Windows.Forms.ComboBox LandComboBox;
+        private System.Windows.Forms.Label label3;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator2;
     }
 }
--- DataManagerSystem/DataManagerSystem/Modules/Add_Semester.Designer.cs
+++ DataManagerSystem/DataManagerSystem/Modules/Add_Semester.Designer.cs
@@ -28,21 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Semester));
             this.Cancelbtn = new System.Windows.Forms.Button();
             this.SaveBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SemesterTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bunifuSeparator2 = new Bunifu.Framework.UI.BunifuSeparator();
             this.SuspendLayout();
             // 
             // Cancelbtn
             // 
-            this.Cancelbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.Cancelbtn.BackColor = System.Drawing.Color.White;
             this.Cancelbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Cancelbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cancelbtn.ForeColor = System.Drawing.Color.White;
-            this.Cancelbtn.Location = new System.Drawing.Point(206, 100);
+            this.Cancelbtn.ForeColor = System.Drawing.Color.Black;
+            this.Cancelbtn.Location = new System.Drawing.Point(231, 156);
             this.Cancelbtn.Name = "Cancelbtn";
-            this.Cancelbtn.Size = new System.Drawing.Size(136, 32);
+            this.Cancelbtn.Size = new System.Drawing.Size(112, 32);
             this.Cancelbtn.TabIndex = 14;
             this.Cancelbtn.Text = "Cancel";
             this.Cancelbtn.UseVisualStyleBackColor = false;
@@ -54,9 +57,9 @@
             this.SaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SaveBtn.ForeColor = System.Drawing.Color.White;
-            this.SaveBtn.Location = new System.Drawing.Point(15, 100);
+            this.SaveBtn.Location = new System.Drawing.Point(117, 156);
             this.SaveBtn.Name = "SaveBtn";
-            this.SaveBtn.Size = new System.Drawing.Size(136, 32);
+            this.SaveBtn.Size = new System.Drawing.Size(112, 32);
             this.SaveBtn.TabIndex = 13;
             this.SaveBtn.Text = "Save";
             this.SaveBtn.UseVisualStyleBackColor = false;
@@ -66,7 +69,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Location = new System.Drawing.Point(24, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 16);
             this.label1.TabIndex = 12;
@@ -74,22 +77,50 @@
             // 
             // SemesterTextBox
             // 
-            this.SemesterTextBox.Location = new System.Drawing.Point(117, 27);
+            this.SemesterTextBox.Location = new System.Drawing.Point(117, 91);
             this.SemesterTextBox.Name = "SemesterTextBox";
-            this.SemesterTextBox.Size = new System.Drawing.Size(225, 20);
+            this.SemesterTextBox.Size = new System.Drawing.Size(226, 20);
             this.SemesterTextBox.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.label3.Location = new System.Drawing.Point(12, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 24);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Add Semerster";
+            // 
+            // bunifuSeparator2
+            // 
+            this.bunifuSeparator2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.bunifuSeparator2.LineThickness = 2;
+            this.bunifuSeparator2.Location = new System.Drawing.Point(12, 29);
+            this.bunifuSeparator2.Name = "bunifuSeparator2";
+            this.bunifuSeparator2.Size = new System.Drawing.Size(416, 10);
+            this.bunifuSeparator2.TabIndex = 23;
+            this.bunifuSeparator2.Transparency = 255;
+            this.bunifuSeparator2.Vertical = false;
             // 
             // Add_Semester
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 157);
+            this.ClientSize = new System.Drawing.Size(437, 253);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bunifuSeparator2);
             this.Controls.Add(this.Cancelbtn);
             this.Controls.Add(this.SaveBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.SemesterTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Add_Semester";
-            this.Text = "Add_Semester";
+            this.Text = "New Semester";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,5 +132,7 @@
         private System.Windows.Forms.Button SaveBtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox SemesterTextBox;
+        private System.Windows.Forms.Label label3;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator2;
     }
 }
