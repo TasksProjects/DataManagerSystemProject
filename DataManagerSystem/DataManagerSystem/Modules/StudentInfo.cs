59a0fcb4835c30fead8e23379d0ce2b3b4491086
Tatkeu T. Ulrich Q <qtchoudji@yahoo.com>
2018-05-27 15:37:14 +0200

    Add StudentInfo.cs

---
+++ DataManagerSystem/DataManagerSystem/Modules/StudentInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataManagerSystem.Modules
+{
+    public partial class StudentInfo : Form
+    {
+        public StudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Vorname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void geschlecht_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nationalitaet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
